e7cc3b09d5ba1458a750deb78c83653b2bb49cb2
Nikhil Sharma <nikhilsharmamtr01@gmail.com>
2024-12-31 23:58:47 +0530

    Updates

--- src/components/GradientBackground.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const GradientBackground: React.FC = () => {
-  return (
-    <div className="absolute inset-0 bg-gradient-radial from-secondary-500/20 via-transparent to-transparent" />
-  );
-};
-
-export default GradientBackground;
